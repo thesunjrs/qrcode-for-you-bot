c30f616efff52b52415261b7d571d5b641fa0d17
Sourcery AI <>
2023-11-14 22:15:41 +0000

    'Refactored by Sourcery'

--- log.py
+++ log.py
@@ -4,10 +4,10 @@ def log(log_message):
     # exit funciton if it should not send logs
     if not send_logs:
         return
-    
-    log_bot_url = "https://api.telegram.org/bot" + bot_api + "/"
+
+    log_bot_url = f"https://api.telegram.org/bot{bot_api}/"
     log_channel_id = os.environ['LOG_CHANNEL_ID'] # 'LOG_CHANNEL_ID' should be present in env
-    log = requests.post(log_bot_url + "sendMessage", data={
-        "chat_id": log_channel_id,
-        "text": log_message
-    })
+    log = requests.post(
+        f"{log_bot_url}sendMessage",
+        data={"chat_id": log_channel_id, "text": log_message},
+    )
